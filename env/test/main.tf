c8510771f6a297649af53de5f61de3e29818abff
Naga <naga@localhost.localdomain>
2025-11-20 16:21:29 +0530

    adding test file

---
+++ env/test/main.tf
@@ -0,0 +1,35 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "docker" {}
+
+# Pull SQL Server container image
+resource "docker_image" "sqlserver" {
+  name         = "mcr.microsoft.com/mssql/server:2022-latest"
+  keep_locally = true
+}
+
+# Create SQL Server container
+resource "docker_container" "sqlserver" {
+  name  = "sqlserver-container2"
+  image = docker_image.sqlserver.name
+
+  env = [
+    "ACCEPT_EULA=Y",
+    "SA_PASSWORD=StrongPassword123!"
+  ]
+
+  # Map internal container port 1433 to a host port (e.g., 1434)
+  ports {
+    internal = 1433
+    external = 1478
+  }
+
+  restart = "always"
+}
