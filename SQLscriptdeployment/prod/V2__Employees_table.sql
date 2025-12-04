662f33802fe949ee7e0df57fdb714e2885acf477
Naga <naga@localhost.localdomain>
2025-12-04 12:58:15 +0530

    Deploying SQL tables on prod

---
+++ SQLscriptdeployment/prod/V2__Employees_table.sql
@@ -0,0 +1,10 @@
+use Demo;
+go
+CREATE TABLE Employees (
+    EmployeeID INT IDENTITY(1,1) PRIMARY KEY,
+    FirstName NVARCHAR(50),
+    LastName NVARCHAR(50),
+    Age INT,
+    HireDate DATE
+);
+
